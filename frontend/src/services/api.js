bce8c09e60457a76679a937210ba4a835b63629e
Nisarga-Astakar <nisargaastkar@gmail.com>
2026-09-11 14:00:36 +0530

    Connect frontend to Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://budgetbuddy-5-production.up.railway.app",
     headers: {
         "Content-Type": "application/json",
     },
